5f629c6fea6edb1cd97c4858b76807dd6d228c9a
anji907 <anji.sk129@gmail.com>
2025-10-29 20:24:16 +0900

    イベント名を変更

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const element = document.getElementById('sample-btn');
 element.addEventListener('click', () => {
     console.log('Button is clicked!');
-    repro.track("WebViewでイベントがトラッキングされました！");
+    repro.track("海外航空券+ホテル");
 });
