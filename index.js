535adb1f385b60a99d98bb7bacec5bfb6619cd7e
anji907 <anji.sk129@gmail.com>
2025-10-29 20:27:15 +0900

    イベント名を変更

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const element = document.getElementById('sample-btn');
 element.addEventListener('click', () => {
     console.log('Button is clicked!');
-    repro.track("海外航空券+ホテル");
+    repro.track("【画面】海外航空券+ホテル_詳細画面表示");
 });
